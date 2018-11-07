37ab1fbf5087e0ca75760d0e5869a8427d6741c4
Hogen Jaing <hogenjiang@hotmail.com>
2018-11-07 02:34:01 -0500

    Signuo interface set up

--- HSEmployeeManager/src/Hogen/login.java
+++ HSEmployeeManager/src/Hogen/login.java
@@ -1,8 +1,12 @@
 package Hogen;
 
-import Tabs.AddItemTab;
+import javafx.animation.ParallelTransition;
+import javafx.animation.ScaleTransition;
+import javafx.animation.SequentialTransition;
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -22,6 +26,7 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 public class login extends Application{
 	
 
@@ -95,12 +100,48 @@ public class login extends Application{
 		        }
 		});
 		
-			//Animation
-	//		ScaleTransition Transition = new ScaleTransition(Duration.millis(500), signup); 
-	//		Transition.setByX(.8);
-	//		Transition.setByY(.8);
-	//		Transition.setAutoReverse(true); 
-	//		Transition.play(); 
+		
+		//Sign up
+		//1
+		Button back = new Button("Back");
+		back.setStyle("-fx-font-size: 16; -fx-base: #2c5185; -fx-background-color: #2c5185; -fx-border-radius: 5;");
+		back.addEventHandler(MouseEvent.MOUSE_ENTERED, 
+			    new EventHandler<MouseEvent>() {
+	        @Override public void handle(MouseEvent e) {
+	        	back.setEffect(shadow);
+	        }
+	});
+		back.addEventHandler(MouseEvent.MOUSE_EXITED, 
+			    new EventHandler<MouseEvent>() {
+
+	        @Override public void handle(MouseEvent e) {
+	        	back.setEffect(null);
+	        }
+	});
+		
+		//2
+		Button signUp = new Button("Sign Up");
+		signUp.setStyle("-fx-font-size: 16; -fx-base: #2c5185; -fx-background-color: #2c5185; -fx-border-radius: 5;");
+		signUp.addEventHandler(MouseEvent.MOUSE_ENTERED, 
+			    new EventHandler<MouseEvent>() {
+	        @Override public void handle(MouseEvent e) {
+	        	signUp.setEffect(shadow);
+	        }
+	});
+		signUp.addEventHandler(MouseEvent.MOUSE_EXITED, 
+			    new EventHandler<MouseEvent>() {
+
+	        @Override public void handle(MouseEvent e) {
+	        	signUp.setEffect(null);
+	        }
+	});
+		
+		TextField email = new TextField();
+        email.setPromptText("Email");
+        email.setStyle("-fx-background-color: #ffffff;");
+        
+        
+			
 		
 		//Pane
 		HBox hbox = new HBox(imageview);
@@ -116,6 +157,7 @@ public class login extends Application{
 		grid.add(tip, 2, 2); //horizontal, vertical
 		grid.add(signup, 3, 2);
 		
+		
 		root.getChildren().add(background1);
 		root.setTop(hbox);
 		root.setCenter(vbox);
@@ -123,24 +165,91 @@ public class login extends Application{
 		
 
 		//function
-				login.setOnAction(new EventHandler<ActionEvent>() {
-		            @Override public void handle(ActionEvent e) {
+		
 
+				login.setOnMousePressed(new EventHandler<Event>() {
 
+					@Override
+					public void handle(Event event) {
 						primaryStage.setScene(scene2);
 						primaryStage.setResizable(false);
 						primaryStage.show();
-		            }
-		        });
+						
+					}
+					
+				});
 				
 				signup.setOnAction(new EventHandler<ActionEvent>() {
 		            @Override public void handle(ActionEvent e) {
-		  
+		            	
+		            	
+		
+		             hbox.getChildren().removeAll(imageview);
+		             
+		             
+		             vbox.getChildren().removeAll(login,username, password);
+		             vbox.getChildren().addAll(email);
+		             
+		             grid.getChildren().removeAll(tip,signup);
+		             
+		             grid.setHgap(6);
+		             grid.add(back, 2, 2);
+		     		 grid.add(signUp, 3, 2);
+		     		 
 
-		            	   
+		             
+
+				     TranslateTransition t1 = new TranslateTransition(Duration.millis(200), username); 		     
+				     t1.setByY(-30);
+				     
+				     TranslateTransition t2 = new TranslateTransition(Duration.millis(200), password); 		     
+				     t2.setByY(-30);
+				     
+				     TranslateTransition t3 = new TranslateTransition(Duration.millis(200), email); 		     
+				     t3.setByY(-30);
+		
+				     
+				     //Order - SequentialTransition, same time - ParallelTransition
+				     ParallelTransition pt = new ParallelTransition(t1, t2, t3);
+				        pt.play();   
 		            	
 		            }
 		        });
+				
+				
+				
+				back.setOnAction(new EventHandler<ActionEvent>() {
+		            @Override public void handle(ActionEvent e) {
+		            	vbox.getChildren().remove(email);
+		            	grid.getChildren().removeAll(signUp, back);
+		            	
+		        		hbox.getChildren().add(imageview);
+		            	
+		   
+		            	 TranslateTransition tb1 = new TranslateTransition(Duration.millis(200), username); 		     
+					     tb1.setByY(30);
+					     
+					     TranslateTransition tb2 = new TranslateTransition(Duration.millis(200), password); 		     
+					     tb2.setByY(30);
+					     
+					     TranslateTransition tb3 = new TranslateTransition(Duration.millis(200), login); 		     
+					     tb3.setByY(100);
+					     
+					     TranslateTransition tb4 = new TranslateTransition(Duration.millis(200), tip); 		     
+					     tb4.setByY(400);
+					     
+					     TranslateTransition tb5 = new TranslateTransition(Duration.millis(200), signup); 		     
+					     tb5.setByY(400);
+					     
+					
+					     //Order - SequentialTransition, same time - ParallelTransition
+					     ParallelTransition ptb = new ParallelTransition(tb1, tb2, tb3, tb4, tb5);
+					        ptb.play();   
+		            	
+		            }
+		        });
+				
+				
 
 
 
@@ -169,13 +278,13 @@ public class login extends Application{
 		TabPane tabPane = new TabPane();
 		tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
 		//Three tabs
-		AddItemTab addItemTab = AddItemTab.getInstance();
-		addItemTab.setClosable(false);
+//		AddItemTab addItemTab = AddItemTab.getInstance();
+//		addItemTab.setClosable(false);
 		//RemoveItemTab removeItemTab = RemoveItemTab.getInstance();
 		//removeItemTab.setClosable(false);
 		//StatisticsTab statisticsTab = StatisticsTab.getInstance();
 		//statisticsTab.setClosable(false);
-		tabPane.getTabs().addAll(addItemTab);
+//		tabPane.getTabs().addAll(addItemTab);
 
 
 		BorderPane pane = new BorderPane();
@@ -190,7 +299,7 @@ public class login extends Application{
 		primaryStage.setResizable(false);
 		primaryStage.show();
 
-        
+		
 		
 		
 	}
