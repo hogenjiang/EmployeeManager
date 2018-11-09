d79943baff85f5112e8396f5e75b54e3bb51aa37
hogenjiang <hogenjiang@hotmail.com>
2018-11-09 15:53:25 -0500

    M

--- HSEmployeeManager/src/Tabs/AddItemTab.java
+++ HSEmployeeManager/src/Tabs/AddItemTab.java
@@ -22,16 +22,17 @@ public class AddItemTab extends Tab{
 		pane.setHgap(10);
 		Text name = new Text("Name");
 		pane.add(name, 0, 0);
+		
 		TextField employeeName = new TextField();
+		employeeName.getStyleClass().add("TextField");
 		pane.add(employeeName, 1, 0);
 		
 		
 		Text year = new Text("Year");
 		pane.add(year, 0, 1);
 		DatePicker date = new DatePicker();
+		date.getStyleClass().add("TextField");
 		pane.add(date, 1, 1);
-		
-
 
 		this.setContent(pane);
 		
